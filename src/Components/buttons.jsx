d31fba2cf92b84e1a0d25894dc6182544a1e7267
Sanidhya Dobhal <sanidhya.dobhal.getsuperbrain.app>
2024-08-14 01:30:48 +0530

    Refactor: Changing certain variable's declaration from let to const

--- src/Components/buttons.jsx
+++ src/Components/buttons.jsx
@@ -1,6 +1,6 @@
 import styles from "../Css modules/buttons.module.css"
 function Buttons(props){
-    let arr = ['1','2','C','3','4','+','5','6','-','7','8','*','9','0','/','.','='];
+    const arr = ['1','2','C','3','4','+','5','6','-','7','8','*','9','0','/','.','='];
     let count = 0;
     return<div id = {styles["btn_grd"]}>
     {arr.map((val)=>{
